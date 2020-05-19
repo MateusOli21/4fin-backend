fcb966dceb8483a80046baf1907741b08b45ce0c
MateusOli21 <mateus.oli.abreu@hotmail.com>
2020-05-19 20:08:48 -0300

    Added JWT and created sessions route. Also added update method to user.

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,18 @@
 const { Router } = require('express');
 
 const UserController = require('./app/controllers/UserController');
+const SessionController = require('./app/controllers/SessionController');
+
+const authMiddleware = require('./app/middlewares/auth');
 
 const routes = new Router();
 
-routes.get('/users', UserController.store);
+routes.post('/users', UserController.store);
+
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
 
 module.exports = routes;
